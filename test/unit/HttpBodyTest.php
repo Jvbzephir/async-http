11f65c6c72bc04f0af95774dd3f4aa75780afda0
martinschroeder <m.schroeder2007@gmail.com>
2016-04-22 11:04:30 +0200

    Re-enabled HTTP server test case.

--- test/unit/HttpBodyTest.php
+++ test/unit/HttpBodyTest.php
@@ -204,10 +204,6 @@ class HttpBodyTest extends \PHPUnit_Framework_TestCase
      */
     public function testHttp1Server(bool $chunked)
     {
-        if (isset($_SERVER['TRAVIS'])) {
-            return $this->markTestSkipped('Test segfaults on in build');
-        }
-        
         $executor = $this->createExecutor();
         
         $executor->runCallback(function () use ($chunked, $executor) {
