4d1002ab1ab30f7b0c88110e697451438f25dea2
Martin Schröder <m.schroeder2007@gmail.com>
2016-05-26 22:25:29 +0200

    Ensuring enough data is read to validate HTTP/2 connection preface.

--- src/Http2/Http2Driver.php
+++ src/Http2/Http2Driver.php
@@ -217,7 +217,7 @@ class Http2Driver implements HttpDriverInterface, HttpUpgradeHandlerInterface
             
             $conn = new Connection(Connection::MODE_SERVER, $socket, $this->context, yield eventEmitter(), $this->logger);
             
-            $preface = yield from IO::readBuffer($socket, strlen(Connection::PREFACE));
+            $preface = yield from IO::readBuffer($socket, strlen(Connection::PREFACE), true);
             
             if ($preface !== Connection::PREFACE) {
                 if ($this->logger) {
@@ -261,7 +261,7 @@ class Http2Driver implements HttpDriverInterface, HttpUpgradeHandlerInterface
     protected function upgradeConnectionDirect(BufferedDuplexStreamInterface $socket, HttpRequest $request, HttpResponse $response, HttpEndpoint $endpoint, callable $action)
     {
         try {
-            $preface = yield from IO::readBuffer($socket, strlen(Connection::PREFACE_BODY));
+            $preface = yield from IO::readBuffer($socket, strlen(Connection::PREFACE_BODY), true);
             
             if ($preface !== Connection::PREFACE_BODY) {
                 if ($this->logger) {
