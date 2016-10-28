3ca2d999be3b489a860fceb449d4867ce4e11230
martinschroeder <m.schroeder2007@gmail.com>
2016-10-28 15:08:09 +0200

    Added strict types directive.

--- src/Http1/Driver.php
+++ src/Http1/Driver.php
@@ -1,13 +1,15 @@
 <?php
 
 /*
- * This file is part of KoolKode Async HTTP.
+ * This file is part of KoolKode Async Http.
  *
  * (c) Martin Schröder <m.schroeder2007@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types = 1);
 
 namespace KoolKode\Async\Http\Http1;
 
