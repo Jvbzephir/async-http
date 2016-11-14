f287d763f27cbac637d11ec11fc03385044ffa52
martinschroeder <m.schroeder2007@gmail.com>
2016-11-14 14:24:39 +0100

    Switched to new logger in example code.

--- example/server.php
+++ example/server.php
@@ -21,15 +21,19 @@ use KoolKode\Async\Http\Middleware\ContentEncoder;
 use KoolKode\Async\Http\Middleware\PublishFiles;
 use KoolKode\Async\Http\Response\FileResponse;
 use KoolKode\Async\Http\WebSocket\ConnectionHandler;
-use KoolKode\Async\Test\TestLogger;
+use KoolKode\Async\Log\PipeLogHandler;
+use KoolKode\Async\Loop\LoopConfig;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/websocket.php';
 
 Loop::execute(function () {
+    $logger = LoopConfig::getLogger();
+    $logger->addHandler(new PipeLogHandler());
+    
     $websocket = new ExampleEndpoint();
     
-    $endpoint = new HttpEndpoint('0.0.0.0:8888', 'localhost', $logger = new TestLogger(STDERR));
+    $endpoint = new HttpEndpoint('0.0.0.0:8888', 'localhost', $logger);
     $endpoint->setCertificate(__DIR__ . '/localhost.pem');
     
     $endpoint->addDriver(new Http2Driver(null, $logger));
