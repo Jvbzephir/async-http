50caf36eab4395d6a6a96d1dfcb8fe587bb93546
Martin Schröder <m.schroeder2007@gmail.com>
2016-11-19 13:42:16 +0100

    Tuned default HPACK indexing settings.

--- src/Http2/HPackContext.php
+++ src/Http2/HPackContext.php
@@ -38,10 +38,28 @@ class HPackContext
             'cache-control',
             'content-type',
             'dnt',
+            'expect',
             'host',
+            'max-forwards',
+            'origin',
             'pragma',
             'user-agent',
+            'x-forwarded-proto',
             'x-requested-with'
+        ];
+        
+        static $never = [
+            'authorization',
+            'date',
+            'if-match',
+            'if-modified-since',
+            'if-none-match',
+            'if-range',
+            'if-unmodified-since',
+            'proxy-authorization',
+            'range',
+            'referer',
+            'x-forwarded-for'
         ];
         
         $context = new HPackContext();
@@ -50,28 +68,53 @@ class HPackContext
             $context->encodings[$name] = self::ENCODING_INDEXED;
         }
         
+        foreach ($never as $name) {
+            $context->encodings[$name] = self::ENCODING_NEVER_INDEXED;
+        }
+        
         return $context;
     }
 
     public static function createServerContext(): HPackContext
     {
         static $indexed = [
+            'accept-ranges',
             'cache-control',
             'content-encoding',
+            'content-language',
             'content-type',
             'p3p',
             'server',
             'vary',
+            'via',
             'x-frame-options',
             'x-xss-protection',
             'x-content-type-options',
+            'x-powered-by',
             'x-ua-compatible'
+        ];
+        
+        static $never = [
+            'age',
+            'content-range',
+            'date',
+            'etag',
+            'expires',
+            'last-modified',
+            'location',
+            'proxy-authenticate',
+            'set-cookie',
+            'www-authenticate'
         ];
         
         $context = new HPackContext();
         
         foreach ($indexed as $name) {
             $context->encodings[$name] = self::ENCODING_INDEXED;
+        }
+        
+        foreach ($never as $name) {
+            $context->encodings[$name] = self::ENCODING_NEVER_INDEXED;
         }
         
         return $context;
